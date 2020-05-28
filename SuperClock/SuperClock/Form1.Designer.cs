4c7c784594555b9804be4824dcdd3ae4a63fea60
denozavrus <42946223+denozavrus@users.noreply.github.com>
2020-05-28 22:00:37 +0300

    Add files via upload

---
+++ SuperClock/SuperClock/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace SuperClock1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 422);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Начать отсчет!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 82);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите свою дату";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите свое время";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(12, 208);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(458, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 33);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(525, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 33);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(684, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 33);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(458, 278);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 33);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(562, 278);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 33);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(658, 278);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 33);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(451, 346);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 17);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Часов";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(555, 346);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(49, 17);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Минут";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(644, 346);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(55, 17);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "Секунд";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(520, 227);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(132, 17);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "Время сверхлюдей";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(528, 42);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(124, 17);
+            this.label13.TabIndex = 15;
+            this.label13.Text = "Дата сверхлюдей";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 422);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 63);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Прекратить(";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 333);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 17;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(12, 304);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(173, 17);
+            this.label14.TabIndex = 18;
+            this.label14.Text = "Коэффициент ускорялки";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(779, 498);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ SuperClock/SuperClock/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SuperClock1
+{
+    public partial class Form1 : Form
+    {
+       Timer timer; 
+       private void Execute (Timer timer, TimeCounters counters, IClocks clocks)
+        {
+            timer.Stop();
+            counters.Hours = 0;
+            counters.Minuts = 0;
+            counters.Seconds = 0;
+            clocks.HoursInDay = 24;
+            clocks.MinutsInHour = 60;
+            clocks.SecondsInMinute = 60;
+            clocks.date = new DateTime(0, 0, 0, 0, 0, 0); 
+        }
+
+        public void TimerTick(SuperClock superClock36, Dictionary<int, string> months)
+        {
+            superClock36.DateCheck(superClock36);
+            label6.Text = superClock36.counters.Hours.ToString();
+            label7.Text = superClock36.counters.Minuts.ToString();
+            label8.Text = superClock36.counters.Seconds.ToString();
+            label3.Text = superClock36.date.Day.ToString();
+            label4.Text = months[(superClock36.date.Month)].ToString();
+            label5.Text = superClock36.date.Year.ToString();
+            superClock36.counters.Seconds += 1; 
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double koef = 1;
+            if (textBox1.Text != null)
+            {
+                try { koef = 1 / Convert.ToDouble(textBox1.Text); }
+                catch
+                {
+                    MessageBox.Show("Пожалуйста укажите число в коэффициенте ускорялки (дробные числа указывайте через запятую)");
+                }
+            }
+            Dictionary<int, string> months = new Dictionary<int, string>(12);
+            months.Add(1, "Января");
+            months.Add(2, "Февраля");
+            months.Add(3, "Марта");
+            months.Add(4, "Апреля");
+            months.Add(5, "Мая");
+            months.Add(6, "Июня");
+            months.Add(7, "Июля");
+            months.Add(8, "Августа");
+            months.Add(9, "Сентября");
+            months.Add(10, "Октября");
+            months.Add(11, "Ноября");
+            months.Add(12, "Декабря");
+            TimeCounters counters = new TimeCounters(dateTimePicker2.Value.Hour, dateTimePicker2.Value.Minute, dateTimePicker2.Value.Second);
+            SuperClock superClock36 = new SuperClock(60, 60, 36, dateTimePicker1.Value,counters);
+            timer = new Timer();
+            timer.Interval = Convert.ToInt32(1000 * koef);
+            timer.Enabled = true;
+            timer.Tick += (x, y) => { TimerTick(superClock36, months); };
+            timer.Start();
+            button1.Enabled = false;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            timer.Stop();
+            button1.Enabled = true; 
+        }
+    }
+    public class TimeCounters
+    {
+        public int Hours { get; set; }
+        public int Minuts { get; set; }
+        public int Seconds { get; set; }
+
+        public TimeCounters (int x, int y, int z)
+        {
+            this.Hours = x;
+            this.Minuts = y;
+            this.Seconds = z; 
+        }
+    }
+
+    public interface IClocks
+    {
+        int HoursInDay { get; set; }
+        int MinutsInHour { get; set; }
+        int SecondsInMinute { get; set; }
+        void DateCheck(IClocks clocks);
+        DateTime date { get; set; }
+    }
+
+    public class SuperClock : IClocks
+    {
+        public TimeCounters counters;
+        public SuperClock(int x, int y, int z, DateTime date, TimeCounters counters)
+        {
+            this.SecondsInMinute = x;
+            this.MinutsInHour = y;
+            this.HoursInDay = z;
+            this.counters = counters; 
+            this.date = date;
+        }
+        public void DateCheck(IClocks superClock)
+        {
+            if (counters.Hours == HoursInDay)
+            {
+                this.date = this.date.AddDays(1);
+                counters.Hours = 0;
+            }
+
+            if (counters.Minuts == MinutsInHour)
+            {
+                counters.Hours++;
+                counters.Minuts = 0;
+            }
+
+            if (counters.Seconds == SecondsInMinute)
+            {
+                counters.Minuts++;
+                counters.Seconds = 0;
+            }
+        }
+
+        public int HoursInDay { get; set; }
+        public int MinutsInHour { get; set; }
+        public int SecondsInMinute { get; set; }
+        public DateTime date { get; set; }
+    }
+}
+
+
