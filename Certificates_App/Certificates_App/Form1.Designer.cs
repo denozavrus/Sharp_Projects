dbae3109abb7eab716898d632ca2d550606b99bd
denozavrus <42946223+denozavrus@users.noreply.github.com>
2020-10-23 09:00:25 +0300

    Add files via upload

---
+++ Certificates_App/Certificates_App/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Certificates_App
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose (bool disposing)
+		{
+			if (disposing && ( components != null ))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent ()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.Select_Excel = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.FIO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Level = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Add = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// Select_Excel
+			// 
+			this.Select_Excel.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.Select_Excel.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Select_Excel.Location = new System.Drawing.Point(6, 110);
+			this.Select_Excel.Name = "Select_Excel";
+			this.Select_Excel.Size = new System.Drawing.Size(197, 69);
+			this.Select_Excel.TabIndex = 0;
+			this.Select_Excel.Text = "Загрузить файл с данными";
+			this.Select_Excel.UseVisualStyleBackColor = false;
+			this.Select_Excel.Click += new System.EventHandler(this.Select_Excel_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FIO,
+            this.Level,
+            this.Date});
+			this.dataGridView1.Location = new System.Drawing.Point(12, 203);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(599, 273);
+			this.dataGridView1.TabIndex = 1;
+			// 
+			// FIO
+			// 
+			this.FIO.HeaderText = "ФИО";
+			this.FIO.MinimumWidth = 6;
+			this.FIO.Name = "FIO";
+			this.FIO.Width = 125;
+			// 
+			// Level
+			// 
+			this.Level.HeaderText = "Квалификация";
+			this.Level.MinimumWidth = 6;
+			this.Level.Name = "Level";
+			this.Level.Width = 125;
+			// 
+			// Date
+			// 
+			this.Date.HeaderText = "Дата";
+			this.Date.MinimumWidth = 6;
+			this.Date.Name = "Date";
+			this.Date.Width = 125;
+			// 
+			// Add
+			// 
+			this.Add.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.Add.Location = new System.Drawing.Point(14, 357);
+			this.Add.Name = "Add";
+			this.Add.Size = new System.Drawing.Size(272, 66);
+			this.Add.TabIndex = 2;
+			this.Add.Text = "Добавить нового пользователя";
+			this.Add.UseVisualStyleBackColor = false;
+			this.Add.Click += new System.EventHandler(this.Add_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.groupBox1.Controls.Add(this.dateTimePicker1);
+			this.groupBox1.Controls.Add(this.listBox1);
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.Add);
+			this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.groupBox1.Location = new System.Drawing.Point(801, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(292, 471);
+			this.groupBox1.TabIndex = 3;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Данные нового пользователя";
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+			this.dateTimePicker1.Location = new System.Drawing.Point(14, 303);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(206, 27);
+			this.dateTimePicker1.TabIndex = 8;
+			this.dateTimePicker1.Value = new System.DateTime(2020, 10, 14, 0, 0, 0, 0);
+			// 
+			// listBox1
+			// 
+			this.listBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 20;
+			this.listBox1.Items.AddRange(new object[] {
+            "Парикмахер III класса",
+            "Парикмахер II класса",
+            "Парикмахер I класса",
+            "Парикмахер-модельер"});
+			this.listBox1.Location = new System.Drawing.Point(14, 179);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(272, 84);
+			this.listBox1.TabIndex = 7;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(14, 97);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(272, 24);
+			this.textBox1.TabIndex = 6;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(10, 270);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(56, 20);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Дата";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(10, 144);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(147, 20);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Квалификация";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(10, 65);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(52, 20);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "ФИО";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.groupBox2.Controls.Add(this.Select_Excel);
+			this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.groupBox2.Location = new System.Drawing.Point(12, 12);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(208, 185);
+			this.groupBox2.TabIndex = 4;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Работа с Excel";
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.groupBox3.Controls.Add(this.button4);
+			this.groupBox3.Controls.Add(this.button1);
+			this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.groupBox3.Location = new System.Drawing.Point(327, 12);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(284, 185);
+			this.groupBox3.TabIndex = 5;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Работа с Word";
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.button4.Location = new System.Drawing.Point(23, 110);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(239, 69);
+			this.button4.TabIndex = 2;
+			this.button4.Text = "Загрузить сертификаты";
+			this.button4.UseVisualStyleBackColor = false;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.button1.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(23, 26);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(239, 66);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Выбрать директорию";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button2.Location = new System.Drawing.Point(617, 394);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(178, 82);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "Очистить все элементы";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button3.Location = new System.Drawing.Point(618, 297);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(177, 91);
+			this.button3.TabIndex = 7;
+			this.button3.Text = "Удалить выбранный Элемент";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.ClientSize = new System.Drawing.Size(1105, 488);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.dataGridView1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Создание сертификатов";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox3.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button Select_Excel;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn FIO;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Level;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+		private System.Windows.Forms.Button Add;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+	}
+}
+
---
+++ Certificates_App/Certificates_App/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using GemBox.Document;
+
+namespace Certificates_App
+{
+	public partial class Form1 : Form
+	{
+
+		string path_to_certificates = System.IO.Directory.GetCurrentDirectory();
+
+		public Form1 ()
+		{
+			InitializeComponent();
+			listBox1.SelectedIndex = 0;
+		}
+
+		private void Select_Excel_Click (object sender, EventArgs e)
+		{
+			OpenFileDialog OPF = new OpenFileDialog();
+			OPF.Filter = "Файлы с расширением xls|*.xls|Файлы с расширением xlsx|*.xlsx|Файлы с макросом xlsm|*.xlsm";
+
+			if (OPF.ShowDialog() == DialogResult.OK)
+			{
+				Excel_file excel = new Excel_file(OPF.FileName);
+
+				List<Person> people = excel.Load_sheet();
+
+				Grid_View grid_View = new Grid_View(dataGridView1);
+				grid_View.Import_data(people); 
+
+			}
+		}
+
+		private void Add_Click (object sender, EventArgs e)
+		{
+			string name = textBox1.Text.ToString();
+			string cvalif = null;
+			try { cvalif = listBox1.SelectedItem.ToString(); }
+			catch
+			{
+
+			}
+			string date = dateTimePicker1.Text.ToString();
+			dataGridView1.Rows.Add(name, cvalif, date); 
+		}
+
+		private void textBox1_TextChanged (object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click (object sender, EventArgs e)
+		{
+			FolderBrowserDialog FBD = new FolderBrowserDialog();
+			if (FBD.ShowDialog() == DialogResult.OK)
+			{
+				path_to_certificates = FBD.SelectedPath;
+			}
+		}
+
+		private void button2_Click (object sender, EventArgs e)
+		{
+			dataGridView1.Rows.Clear(); 
+		}
+
+		private void button3_Click (object sender, EventArgs e)
+		{
+			int ind = dataGridView1.SelectedCells[0].RowIndex;
+			try
+			{
+				dataGridView1.Rows.RemoveAt(ind);
+			}
+			catch
+			{
+				MessageBox.Show("Выберите элемент для удаления");
+			}
+		}
+
+		private void button4_Click (object sender, EventArgs e)
+		{
+			Grid_View grid_View = new Grid_View(dataGridView1);
+			var people = grid_View.Export_data(); 
+			for (int i = 1; i < people.Count + 1; i++)
+			{
+				string path = path_to_certificates + String.Format(@"\Certificate{0}.docx", i);
+				Word_file.Create_certificate(path, people[i - 1]); 
+			}
+		}
+	}
+
+	public class Person
+	{
+		public Person(string name, string cvalif, string date)
+		{
+			this.name = name;
+			this.Cvalif = cvalif;
+			this.date = date;
+		}
+
+		public Person(List<string> strings)
+		{
+			try
+			{
+				this.name = strings[0];
+				this.Cvalif = strings[1];
+				this.date = strings[2];
+			}
+			catch
+			{
+				throw new ArgumentException("Not enough parameters to create Person");
+			}
+		}
+
+		private List<string> cvalifications = new List<string>() { "Парикмахер III класса", 
+			"Парикмахер II класса", 
+			"Парикмахер I класса", 
+			"Парикмахер-модельер" };
+
+		public string name;
+		private string cvalif;
+		public string date; 
+		public string Cvalif
+		{
+			get
+			{
+				return cvalif; 
+			}
+			set
+			{
+				if (cvalifications.Contains(value))
+				{
+					cvalif = value; 
+				}
+				else
+				{
+					cvalif = null; 
+				}
+			}
+		}
+		public List<string> Prepare_for_load ()
+		{
+			if (this.cvalif == null)
+			{
+				Cvalif = "Парикмахер III класса";
+			}
+			if (this.date == null)
+			{
+				this.date = DateTime.Today.ToString("d.MM.yyyy");
+			}
+			if (this.name == null)
+			{
+				throw new Exception("Error getting name of person"); 
+			}
+			List<string> result = new List<string>();
+			result.Add(this.name);
+			result.Add(this.cvalif);
+			result.Add(this.date);
+			return result; 
+		}
+
+	}
+
+	public class Excel_file
+	{
+		public string file_path;
+
+		public Excel_file (string path)
+		{
+			this.file_path = path;
+		}
+		public static SharedStringItem GetSharedStringItemById (WorkbookPart workbookPart, int id)
+		{
+			return workbookPart.SharedStringTablePart.SharedStringTable.Elements<SharedStringItem>().ElementAt(id);
+		}
+
+		public List<Person> Load_sheet ()
+		{
+			using (SpreadsheetDocument doc = SpreadsheetDocument.Open(file_path, false))
+			{
+				WorkbookPart workbookPart = doc.WorkbookPart;
+
+				List<Person> people = new List<Person>();
+
+				List<string> collected_strings = new List<string>();
+
+				Sheet theSheet = workbookPart.Workbook.Descendants<Sheet>().FirstOrDefault();
+
+				if (theSheet == null)
+				{
+					throw new ArgumentException("sheetName");
+				}
+
+				WorksheetPart wsPart =
+				(WorksheetPart)( workbookPart.GetPartById(theSheet.Id) );
+
+				var rows = wsPart.Worksheet.Descendants<DocumentFormat.OpenXml.Spreadsheet.Row>();
+				foreach (var row in rows)
+				{
+					collected_strings.Clear();
+
+					foreach (DocumentFormat.OpenXml.Spreadsheet.Cell c in row.Elements<DocumentFormat.OpenXml.Spreadsheet.Cell>())
+					{
+
+						string cellValue = string.Empty;
+
+						if (c.DataType != null)
+						{
+							if (c.DataType == CellValues.SharedString)
+							{
+								int id = -1;
+
+								if (Int32.TryParse(c.InnerText, out id))
+								{
+									SharedStringItem item = GetSharedStringItemById(workbookPart, id);
+
+									if (item.Text != null)
+									{
+										cellValue = item.Text.Text;
+									}
+									else if (item.InnerText != null)
+									{
+										cellValue = item.InnerText;
+									}
+									else if (item.InnerXml != null)
+									{
+										cellValue = item.InnerXml;
+									}
+								}
+							}
+						}
+						else
+						{
+							DateTime start_date = new DateTime(1900, 1, 1);
+							try
+							{
+								int days = Convert.ToInt32(c.CellValue.Text);
+								start_date = start_date.AddDays(days - 2);
+								string str_date = start_date.ToString("d.MM.yyyy");
+								cellValue = str_date;
+							}
+							catch
+							{
+								cellValue = null;
+							}
+						}
+
+						collected_strings.Add(cellValue);
+
+					}
+					people.Add(new Person(collected_strings));
+				}
+				people.RemoveAt(0);
+				return people;
+			}
+		}
+	}
+
+	public class Grid_View
+	{
+		public DataGridView gridView; 
+
+		public Grid_View(DataGridView gridView)
+		{
+			this.gridView = gridView; 
+		}
+
+		public void Import_data(List<Person> people)
+		{
+			foreach (Person person in people)
+			{
+				var loaded_person = person.Prepare_for_load();
+				string name = loaded_person[0];
+				string cvalif = loaded_person[1];
+				string date = loaded_person[2];
+				gridView.Rows.Add(name, cvalif, date);
+			}
+		}
+		public List<Person> Export_data()
+		{
+			List<Person> people = new List<Person>();
+			string name;
+			string cvalif;
+			string date; 
+			for (int i = 0; i< gridView.Rows.Count-1; i++)
+			{
+				var row = gridView.Rows[i]; 
+				try
+				{
+					name = row.Cells[0].FormattedValue.ToString();
+					cvalif = row.Cells[1].FormattedValue.ToString();
+					date = row.Cells[2].FormattedValue.ToString();
+				}
+				catch
+				{
+					throw new Exception("Error reading table"); 
+				}
+				people.Add(new Person(name, cvalif, date));
+			}
+			return people; 
+		}
+	}
+
+	public class Word_file
+	{
+		public static void Create_certificate (string path, Person person)
+		{
+			string startupPath = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())) + @"\Samples\shablon-sertificata-03.docx";
+			foreach (var process in Process.GetProcessesByName("winword"))
+			{
+				process.Kill();
+			}
+
+			DocumentModel doc = null;
+			ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+			var loaded_person = person.Prepare_for_load();
+			try
+			{
+				File.Copy(startupPath, path, true);
+					if (File.Exists(path))
+					{
+						doc = DocumentModel.Load(path);
+						BookmarkCollection wBookmarks = doc.Bookmarks;
+						ContentRange wRange;
+						int i = 0;
+						foreach (var mark in wBookmarks)
+						{
+							wRange = mark.GetContent(false);
+							wRange.LoadText(loaded_person[i].ToString());
+							i++;
+						}
+					doc.Save(path);
+					doc = null;
+					}
+			}
+			catch 
+			{
+				MessageBox.Show("Во время выполнения произошла ошибка!");
+			}
+		}
+	}
+
+}
